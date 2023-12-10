784f7648956a4b7c2855a6a9124e20544ee7f5ca
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2023-12-10 11:56:31 +0300

    first textlabel added

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ window = tkinter.Tk()
 window.title("BMI Calculator")
 window.minsize(width=200,height=200)
 
-label = tkinter.Label(text= "Enter Your Weight(kg)")
-label.pack()
 
+label1 = tkinter.Label(text= "Enter Your Weight(kg)")
+label1.pack()
 
+text1 = tkinter.Text(width=15,height=2)
+text1.pack()
 
 window.mainloop()
