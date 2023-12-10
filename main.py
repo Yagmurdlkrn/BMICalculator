34c536e6dac6b18b87a162595318bbd976e0f7af
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2023-12-10 12:47:57 +0300

    functionand label3 added

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("BMI Calculator")
-window.minsize(width=200,height=200)
+window.minsize(width=250,height=250)
 
-
-label1 = tkinter.Label(text= "Enter Your Weight(kg)")
+label1 = Label(text= "Enter Your Weight(kg)")
 label1.pack()
 
-text1 = tkinter.Text(width=15,height=2)
+text1 = Text(width=15,height=2)
 text1.pack()
 
-label2 = tkinter.Label(text= "Enter Your Height(cm)")
+label2 = Label(text= "Enter Your Height(cm)")
 label2.pack()
 
-text2 = tkinter.Text(width=15,height=2)
+text2 = Text(width=15,height=2)
 text2.pack()
 
-button = tkinter.Button(text="Calculate")
+def button_clicked():
+
+    weight = float(text1.get("1.0", END).strip())
+    height = float(text2.get("1.0", END).strip())
+
+    bmi = weight / (height / 100) ** 2
+
+    if bmi < 18.5:
+        label3.config(text=f"Your BMI is {bmi: .2f}.You are under weight")
+    elif bmi >= 18.5 and bmi <25:
+        label3.config(text=f"Your BMI is {bmi: .2f}.You are normal weight")
+    elif bmi >= 25 and bmi <30:
+        label3.config(text=f"Your BMI is {bmi: .2f}.You are over weight")
+    elif bmi >= 30 and bmi <35:
+        label3.config(text=f"Your BMI is {bmi: .2f}.You are Obesity (Class I)")
+    elif bmi >= 35 and bmi < 40:
+        label3.config(text=f"Your BMI is {bmi: .2f}.You are Obesity (Class II)")
+    else:
+        label3.config(f"Your BMI is {bmi: .2f}.You are Extreme Obesity")
+
+button = Button(text="Calculate",command=button_clicked)
 button.pack()
 
-
+label3 = Label()
+label3.pack()
 
 window.mainloop()
